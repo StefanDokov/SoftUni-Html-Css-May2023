20991f940a6afd5962111edb32a036e12044867b
StefanDokov <stefandokov2211@gmail.com>
2023-08-15 18:31:08 +0300

    add more stuff
    ul images

---
+++ pfolder/js/script.js
@@ -0,0 +1,15 @@
+const slider = document.querySelector('.slider');
+const sliderList = document.querySelector('.slider-list');
+const images = document.querySelectorAll('.slider-list li');
+
+let currentIndex = 0;
+
+slider.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % images.length;
+  updateSliderPosition();
+});
+
+function updateSliderPosition() {
+  const slideWidth = images[currentIndex].clientWidth;
+  sliderList.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+}
