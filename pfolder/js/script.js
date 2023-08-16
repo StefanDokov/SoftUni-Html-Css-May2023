46125a534d39a5d3e9b8994b06bd7119ace256b3
StefanDokov <stefandokov2211@gmail.com>
2023-08-16 19:52:28 +0300

    about
    about section

--- pfolder/js/script.js
+++ pfolder/js/script.js
@@ -13,3 +13,8 @@ function updateSliderPosition() {
   const slideWidth = images[currentIndex].clientWidth;
   sliderList.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 }
+
+const textz = document.querySelector('.texter p');
+textz.innerHTML = textz.innerText.split("").map(
+  (char, i) => `<span style="transform:rotate(${i * 14}deg)">${char}</span>`
+).join("")
