11239fa3585d830599521fe50e69fe6358f334ec
StefanDokov <stefandokov2211@gmail.com>
2023-09-08 18:46:25 +0300

    dropdown
    add dropdown menu

--- pfolder/js/script.js
+++ pfolder/js/script.js
@@ -38,3 +38,14 @@ backBtn.addEventListener("click", () => {
   scrollContainer.style.scrollBehavior = "smooth";
   scrollContainer.scrollLeft -= 1040;
 });
+
+const toggleBtn = document.querySelector('.toggle_btn');
+        const toggleBtnIcon = document.querySelector('.toggle_btn i');
+        const dropDownMenu = document.querySelector('.dropdown_menu');
+         
+        toggleBtn.onclick = function () {
+            dropDownMenu.classList.toggle('open');
+            const isOpen = dropDownMenu.classList.contains('open');
+
+            toggleBtnIcon.classList = isOpen? 'fa-solid fa-xmark' : 'fa-solid fa-bars';
+        }
